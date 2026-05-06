b90bc0f2589b79ce0a04c6c1e6b8f31d32998b23
Roushan Kumar Sahu <roushankumarsahu20@gmail.com>
2026-05-06 16:49:07 +0530

    Add new files

---
+++ git.js
@@ -0,0 +1,61 @@
+// Git
+// - Free and Open Source Version Control System :- tools that helps to track changes in the code and 
+// collaborate with other developers.
+// It allows multiple developers to work on the same codebase without conflicts and helps to manage 
+// different versions of the code. It also provides features like branching, merging, and reverting changes. 
+
+// GITHUB:- 
+// -- website where we host repositeries online and share with others. 
+// It is a cloud based hosting service for git repositories.
+//  It provides a web interface to manage and collaborate on code projects. 
+// It also provides features like issue tracking, pull requests, and code reviews.
+
+
+
+// commit in github - tells changes in the repo or codebase.
+//  It is a snapshot of the code at a specific point in time.
+
+// REAMDE.md - file that contains information about the project, such as its purpose, how to use it, 
+// and any other relevant details. It is often the first file that users see when they visit a repository.
+
+
+// last commit - the most recent commit made to the repository. 
+// It represents the latest changes that have been made to the codebase.
+
+// USING GIT:- 
+// . Command line interface (CLI) is used to interact with git.
+// -IDE/CODE EDITOR: - Visual Studio Code, IntelliJ IDEA, etc. have built in support for git and provide a graphical interface to manage git repositories.
+// -GRAPHICAL USER INTERFACE (GUI) TOOLS: - GitHub Desktop, Sourcetree, etc. provide a user friendly interface to manage git repositories without using the command line.
+
+
+// 1. git init - initializes a new git repository in the current directory.
+// 2. git add . - adds all the changes in the current directory to the staging area.
+// 3. git commit -m "message" - commits the changes in the staging area with a message describing the changes.
+// 4. git push origin main - pushes the committed changes to the remote repository on GitHub.
+
+// git config --global user.name "roushan" - sets the global username for git.
+// git config --global user.email "roushankumarsahu20@gmail.com" - sets the global email for git.
+
+// cloning a repository - creating a local copy of a remote repository on your computer.
+// It allows you to work on the code locally and then push your changes back to the 
+// remote repository when you're done. 
+// To clone a repository, you can use the command "git clone <repository URL>" in the terminal.
+
+// FILE STATUS IN GIT:-
+// 1. Untracked: - files that are not being tracked by git. They are new files that have been added to
+//  the directory but have not been staged or committed yet.
+// 2. Staged: - files that have been added to the staging area and are ready to be committed.
+// 3. Committed: - files that have been committed to the repository and are part of the project history.
+// 4. Modified: - files that have been changed but have not been staged or committed yet.
+// 5. Deleted: - files that have been removed from the directory but have not been staged or committed yet.
+// 6. unmodified: - files that have not been changed since the last commit.
+//  They are the same as the last committed version of the file.
+// 7. Renamed: - files that have been renamed but have not been staged or committed yet.
+
+
+// BASIC COMMANDS:- 
+// add:- git add <file name> - adds a specific file to the staging area.
+// add karne par staging area me chala jata hai aur commit karne ke liye ready ho jata hai.
+// git add . - adds all the changes in the current directory to the staging area.
+// commit:- git commit -m "message" - it is the record of changes made to the codebase.
+// push:- git push origin main - pushes the committed changes to the remote repository on GitHub.
